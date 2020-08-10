3b84125358654d4ed8b59ff383da62c5ed350147
Islam Mesabah <islamkaloop@gmail.com>
2020-08-10 23:59:47 +0200

    add update app content

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,28 +1,66 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+var firebaseConfig = {
+  apiKey: "AIzaSyCZFtO3a-x6XgeZhrz3_psmHA-NxBM9JpU",
+  authDomain: "todo-listapp-eb8d2.firebaseapp.com",
+  databaseURL: "https://todo-listapp-eb8d2.firebaseio.com",
+  projectId: "todo-listapp-eb8d2",
+  storageBucket: "todo-listapp-eb8d2.appspot.com",
+  messagingSenderId: "1034231435387",
+  appId: "1:1034231435387:web:ffa27b07dbf1881ec512de",
+  measurementId: "G-5XTZFSVNMS"
+};
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+const root = firebase.database().ref('Tasks/');
+
+root.on("value",
+
+  (snapshot) => {
+    const  myNodelist = document.getElementById("tasks");
+
+    myNodelist.innerHTML = ""
+
+    snapshot.forEach((child) => {
+      task = child.val();
+      var li = document.createElement("li");
+      var inputValue = task.name
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+      span.onclick = function() {
+          var div = this.parentElement;
+          console.log(div.id);
+          div.style.display = "none";
+      }
+      li.id = child.key;
+      if(task.status == 'true'){
+        li.classList.toggle('checked');
+      }
+
+      myNodelist.appendChild(li);
+    });
+
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+        console.log(ev.target.id);
+      }
+    }, false);
+
+  },
+
+  (error) => {
+    console.log("Error: " + error.code);
   }
-}, false);
+
+);
 
 function newElement() {
   var li = document.createElement("li");
@@ -41,12 +79,10 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+  span.onclick = function() {
+    var div = this.parentElement;
+    console.log(div.id );
+    div.style.display = "none";
   }
 }
 
